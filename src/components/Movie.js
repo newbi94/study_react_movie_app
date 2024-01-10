8c531b78d30f018b07581ca365c1c60aa3e5b250
newbi94 <152586059+newbi94@users.noreply.github.com>
2024-01-10 17:16:05 +0900

    24.01.10
    fin
    npm run deploy
    publishing on github

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,34 +1,35 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/:${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
-}//<Link>는 기존에 html에서 하던 <a href=""~ 와 다르게
-//리액트의 장점을 살려 Link를 사용하면 페이지 이동 시에 페이지 전체를 새로고침하지 않는다.
-//그리고 {genres.map~ 부분에 key값을 예전에 배운거 써먹는다고 Date.now()
-//를 썼다가 콘솔 창에 잔뜩 오류가 발생했는데, 한번에 불러오기 때문에 아무리
-//현재까지의 밀리초라고 해도 한순간에 찍히는거라 key값이 유니크할 수가 없는 거같다.
-//해서 일단 g로 하는게 의심스러웠지만 다시 g로 바꿔놓자 에러는 없어졌다.
+  );// ${id}앞에 : 를 썼었는데 그게 모든 에러의 원인이었다.
+  //콘솔로그로 useParams를 찍을때 :가 같이 찍히는 에러와 Unexpected end of JSON까지
+  //: 를 지움으로써 해결됨
+}
 Movie.propTypes = {
   id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-  //arrayOf()는 array로써 받을 인자들을 설정하는 것.
+  
 };
 
 export default Movie;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,10 +3,6 @@ import { useParams } from "react-router-dom";
 function Detail() {
   const { id } = useParams();
   
-  //useParams는 App.js에서 <Route path="/movie/:id" element={<Detail />} /> 부분중
-  //: 뒷부분 즉, id부분을 변수로 받는다(?).
-  //Home.js에서 id={movie.id} 와 Movie.js의 <Link to={`/movie/:${id}`}>{title}</Link>
-  //그리고 App.js에서 <Route path="/movie/:id" element={<Detail />} />가 상호작용을 하는 것.
   console.log(id)
   const getMovie = async () => {
     const json = await (
@@ -16,23 +12,11 @@ function Detail() {
     ).json();
     console.log(json);
   };
-  //여기서 나온 fetch주소는 어떤 경로로 알게 된건지.
-  //최초 Home.js에서 사용한 fetch주소를 통해 어떻게 어떻게 하면 알아낼 수 있는거같은데
-  //왜 니꼬는 이런걸 건너띄는지 모르겠다 복잡한가
-  
-  //위 const getMovie~ 코드에서 에러가 난다.
-  //Unexpected end of JSON input
-  //SyntaxError: Unexpected end of JSON input
-  //at getMovie (http://localhost:3000/main.f12bb9d151c530d918a1.hot-update.js:33:97)
   
   useEffect(() => {
     getMovie();
   }, []);
   return <h1>Detail</h1>;
 }
-
-  //Detail로 이끈 것은 App.js에서 <Route path="/movie/:id" element={<Detail />} />
-  //부분인데 여기서 " : " 뒷부분에 쓰인 것들이 변수가 된다.
-  // :ex라고 한다면 useParams을 써서 console.log해주면 ex: ~가 띄워진다.
 
   export default Detail;
